a644f2b8fee9cfa6ba640284c3713b6c55f55412
rupesh <rupeshquto@gmail.com>
2018-06-01 16:31:56 +0530

    All files

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+//init app
+const app = new express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
+
+const verificationController = require('./controllers/verification');
+app.get('/webhook', verificationController);
+const messageWebhookController = require('./controllers/messageWebhook');
+app.post('/webhook', messageWebhookController);
+
+//Page access toket: EAAUlZAuTFadIBAB7GydA5ijvcmXImLvaocyrobfkIYX50RHZCVERhfBjC0IZABbEkqTapwZBaRG7aUFpgQbETynypcy70mC3ylOb35AL8zXbiR2FZBZCdz7eW25iDwVZB5d1IGa5ulMd3EaaZA7WA4lyaZBy7vr1hghmbyG35pc5JpAZDZD
+//EAAUlZAuTFadIBAEzPfNr7UREZAmyqXyYZAnjR8T00dxYHzRFK9RhkE7lgk4gZBgmiWWbzDUpZAsQZCObtFSoFG1GYPZB9lJC3p3liD7rfZAtczzb9MRQ2ftlSLm3HKymCd7A7XlWJmbmZCZB1q6TT1NTZB6UsZBxiNFSaNSt2ZCvxyoNxwQZDZD
+// Call back url
+/*
+Web Interface                 http://127.0.0.1:4041
+Forwarding                    http://592b6664.ngrok.io -> localhost:80
+Forwarding                    https://592b6664.ngrok.io -> localhost:80
+*/
---
+++ helpers/processMessage.js
@@ -0,0 +1,28 @@
+const API_AI_TOKEN = 'd26530ac4d384f2e8d3f2d9cbf64aeea';//‘your Dialogflow Client Access Token’;
+const apiAiClient = require('apiai')(API_AI_TOKEN);
+const FACEBOOK_ACCESS_TOKEN = 'EAAUlZAuTFadIBAGGvrFdMrdKOi22b0dpbIxeNnc1LFanRWtB4ZA8Atz2rTXA6a7JdfOTwrcRlxlMZClxwGHqJOOp2WZCa2Ik3ZBMUOwnd3EpKPkftu0v0HLRAMQu9trZA07ZB63xITN1DpP1DO9wlKiWFih70kDQyErdGNGxeeD4wZDZD';//‘your Facebook Page Access Token’;
+const request = require('request');
+const sendTextMessage = (senderId, text) => {
+ request({
+ url: 'https://graph.facebook.com/v2.6/me/messages',
+ qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+ method: 'POST',
+ json: {
+ recipient: { id: senderId },
+ message: { text },
+ }
+ });
+};
+module.exports = (event) => {
+ const senderId = event.sender.id;
+ const message = event.message.text;
+const apiaiSession = apiAiClient.textRequest(message, {sessionId: 'crowdbotics_bot'});
+apiaiSession.on('response', (response) => {
+ const result = response.result.fulfillment.speech;
+sendTextMessage(senderId, result);
+ });
+apiaiSession.on('error', error => console.log(error));
+ apiaiSession.end();
+};
+
+//EAAUlZAuTFadIBAGGvrFdMrdKOi22b0dpbIxeNnc1LFanRWtB4ZA8Atz2rTXA6a7JdfOTwrcRlxlMZClxwGHqJOOp2WZCa2Ik3ZBMUOwnd3EpKPkftu0v0HLRAMQu9trZA07ZB63xITN1DpP1DO9wlKiWFih70kDQyErdGNGxeeD4wZDZD
